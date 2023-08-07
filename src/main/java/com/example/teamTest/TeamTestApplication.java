19b2a07fb8a34c35f0610de86e1d9d3528dce5af
yoongunyong <backendgun@gmail.com>
2023-08-07 14:16:24 +0900

    initial

---
+++ src/main/java/com/example/teamTest/TeamTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teamTest/TeamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
